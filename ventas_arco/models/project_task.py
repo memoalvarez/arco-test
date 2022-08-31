2ba84295b75244d0b3ea1799db8495148e29ca5b
memoalvarez <memoalvarez713@gmail.com>
2022-08-31 10:47:14 -0500

    [FIX] Correcciones en project_task

--- ventas_arco/models/project_task.py
+++ ventas_arco/models/project_task.py
@@ -15,7 +15,7 @@ class ProjectTask(models.Model):
    guia_id = fields.Many2one('stock.picking', string="Informe")
    chofer = fields.Many2one('res.partner', string="Operador")
    unidad = fields.Many2one('unidades.unidades', string="Unidad")
-   cliente = fields.Many2one('res.partner', string='Cliente')
+   recoleccion = fields.Many2one('res.partner', string='Recolección')
 
 
    project_task_function = fields.Selection([
