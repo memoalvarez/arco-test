b57e1b8a4345716c35816a3f7062eda7cf0f9ec5
memoalvarez <memoalvarez713@gmail.com>
2022-09-01 14:02:44 -0500

    [FIX] Se agrego un metodo para añadir tareas a una orden de venta 05

--- ventas_arco/models/sale_order_cotizacion.py
+++ ventas_arco/models/sale_order_cotizacion.py
@@ -8,14 +8,54 @@ class SaleOrderCotizacion(models.TransientModel):
 
    cotizacion_id = fields.Many2one('sale.order', string="Orden de venta")
 
-   def add_to_report_button(self):
+   def create_report_button(self):
       self.ensure_one()
       tareas = self.env['project.task'].browse(self._context.get('active_ids', []))
+      producto = self.sudo().env['product.product'].search([('default_code', '=', 'EC')])
+      plantilla = self.sudo().env['sale.order.template'].search([('name', '=', 'Envío')])
+      
+      bultos = 0
+      ec_descripcion = ''
 
-      for reg in tareas:
-         reg.sale_order_id = self.cotizacion_id.id
-         reg.action_create_cotizacion()
+      #Se crea diccionario para caratula de cotizacion
+      vals = {
+         'partner_id' : tareas[0].id,
+         'sale_order_template_id': plantilla.id
+      }
+      sale_order = self.env['sale.order'].create(vals)
+      
+      for tarea in tareas:
+         tarea.sale_order_id = sale_order.id
+         for record in tarea.reception_ids:
+            if record.state == 'done':
+               for line in record.move_ids_without_package:
+                  bultos += line.quantity_done
+                  ec_descripcion = ec_descripcion + str(line.description_picking) + "\n"
          
+      seq = 10
+      #Esto sirve para agregar las lineas de la plantilla a la orden
+      for line in plantilla.sale_order_template_line_ids:
+         vals = {
+            'sequence' : seq,
+            'order_id' : sale_order.id,
+            'display_type': line.display_type,
+            'name' : line.name
+         }
+         linea = self.env['sale.order.line'].create(vals)
+         seq += 10
+      
+      vals = {
+            'order_id' : sale_order.id,
+            'sequence' : 15,
+            'product_id' : producto.id,
+            'name': ec_descripcion,
+            'numeroDeBultos': bultos
+      }
+      linea = self.env['sale.order.line'].create(vals)
+      
+      for tarea in tareas:
+         tarea.sale_line_id = linea.id
+
       return {
          "type": "ir.actions.act_window",
          "res_model": "sale.order",
@@ -24,30 +64,4 @@ class SaleOrderCotizacion(models.TransientModel):
          "context": {"create": False},
       }
 
-   def create_report_button(self):
-      self.ensure_one()
-   
-      plantilla2 = self.sudo().env['sale.order.template'].search([('name', '=', 'Envío')])
-      
-      #Se crea diccionario para caratula de cotizacion
-      vals = {
-         'partner_id' : self.partner_id.id,
-         'sale_order_template_id': plantilla2.id
-      }
-      #Se crea cotizacion y se agrega id a tarea
-      sale_order = self.env['sale.order'].create(vals)
-      
-      tareas = self.env['project.task'].browse(self._context.get('active_ids', []))
-
-      for reg in tareas:
-         reg.sale_order_id = sale_order.id
-         reg.action_create_cotizacion()
-         
-      return {
-         "type": "ir.actions.act_window",
-         "res_model": "sale.order",
-         "views": [[False, "form"]],
-         "res_id": sale_order.id,
-         "context": {"create": False},
-      }
 
